e3b0b466a7a804e13931ab39671e87bc579b1146
CodeMaster1901 <abhirup1901@gmail.com>
2023-04-29 04:38:55 +0530

    Add getURL_title() function

--- main.py
+++ main.py
@@ -3,10 +3,30 @@ import requests
 from requests.exceptions import HTTPError
 from rich.console import Console
 from rich.prompt import Prompt
+import urllib.request
+import re
+
 console = Console()
 console.print("Hello, Welcome to GetRFC", style="green on black")
-RFC = Prompt.ask("Enter the RFC Number", default="15")
-console.log(RFC)
+# RFC = Prompt.ask("Enter the RFC Number", default="15")
+# console.log(RFC)
+# page=Prompt.ask("Do you want just the first page?", default="Y")
+# if page.lower() == 'y':
+#     page='fpage'
+# else:
+#     page='full'
+# tofile=Prompt.ask("Do you want to write the output to a file", default="Y")
+# if tofile.lower() == 'y':
+#     tofile=True
+# else:
+#     tofile=False
+# if tofile:
+#     filename = input("Please enter the file name to store RFC at: ")
+# else:
+#     filename = None
+
+title = Prompt.ask("Enter the RFC title")
+console.log(title)
 page=Prompt.ask("Do you want just the first page?", default="Y")
 if page.lower() == 'y':
     page='fpage'
@@ -53,7 +73,31 @@ def getURL(rfc_no):
     # reuturn access URL for a specific RFC no
     return f'https://www.ietf.org/rfc/rfc{rfc_no}.txt'
 
+def getURL_title(title):
+    webUrl=urllib.request.urlopen('https://www.ietf.org/rfc/rfc-index.txt')
+    # print("result:"+str(webUrl.getcode()))
+    data = webUrl.read().decode('utf-8')
+    matches = [i.start() for i in re.finditer(title, data)]
+    
+    if len(matches) == 0 : print("nothing")
+    
+    def get_RFC_id(match):
+        for i in range(match, 1, -1):
+            if data[i] == '\n' and data[i-1] == '\n':
+                return data[i+1:i+5]
+    
+    ids = []
+    for match in matches:
+        ids.append(get_RFC_id(match))
+        # yield runRequest(getURL((get_RFC_id(match))))
 
+
+    for i, id in enumerate(ids):
+        print(f"{i+1}. {id}")
+        
+    choice = Prompt.ask("Enter your choice", default="1")
+    return getURL(ids[int(choice)-1])
+    # return f'https://www.ietf.org/rfc/rfc{ids[int(choice)-1]}.txt'
 
 def runRequest(url):
 
@@ -78,16 +122,85 @@ def runRequest(url):
         raise ForbiddenError
 
 
-runRequest((getURL(RFC)))
+# runRequest((getURL(RFC)))
 
-def getContentFromRFCNo(number, option, tofile, filename):
 
+# def getContentFromRFCNo(number, option, tofile, filename):
+
+#     out = ''
+
+#     if option == 'full':
+
+#         try:
+#             data = runRequest(getURL(number))
+
+#         except Error404:
+#             print("Sorry! The resource you are looking for does not exist!")
+#             return 0
+
+#         except ForbiddenError:
+#             print("Sorry! It appears that you do not have access to the above URL")
+#             return 0
+
+#         out = data.text
+
+
+#     elif option == 'fpage':
+
+#         try:
+#             data = runRequest(getURL(number))
+
+#         except Error404:
+#             print("Sorry! The resource you are looking for does not exist!")
+#             return 0
+
+#         except ForbiddenError:
+#             print("Sorry! It appears that you do not have access to the above URL")
+#             return 0
+
+#         iterable_content = data.iter_lines()
+
+#         buf = ""
+
+#         for line in iterable_content:
+#             line = line.decode('utf-8')
+#             if '[Page 1]' not in line:
+#                 buf += line + '\n'
+#             else:
+#                 buf += line + '\n'
+#                 break
+
+#         out = buf
+
+
+#     if tofile:
+
+#         # we have been asked to write the output data to the file
+
+#         if filename == '' or filename == None:
+#             filename = input("Please enter the file name to store RFC at: ")
+            
+#         with open(filename, 'w+') as current_file:
+#             current_file.write(out)
+
+#         print(f"Successfully written to file {filename} !")
+
+#     else:
+
+#         print(out)
+
+
+# getContentFromRFCNo(RFC, page,tofile, filename)
+        
+runRequest(getURL_title(title))
+#Title
+def getContentFromRFCTitle(title, option, tofile, filename):
     out = ''
 
     if option == 'full':
 
         try:
-            data = runRequest(getURL(number))
+            data = runRequest(getURL_title(title))
 
         except Error404:
             print("Sorry! The resource you are looking for does not exist!")
@@ -103,7 +216,7 @@ def getContentFromRFCNo(number, option, tofile, filename):
     elif option == 'fpage':
 
         try:
-            data = runRequest(getURL(number))
+            data = runRequest(getURL_title(title))
 
         except Error404:
             print("Sorry! The resource you are looking for does not exist!")
@@ -145,7 +258,7 @@ def getContentFromRFCNo(number, option, tofile, filename):
         print(out)
 
 
-getContentFromRFCNo(RFC, page,tofile, filename) 
+getContentFromRFCTitle(title,page,tofile,filename) 
 # if __name__ == '__main__':
 
 #     # setup ArgParse for ease of use
