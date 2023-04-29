bb8e938fb33dc41f3be79d1633710fd460af528c
Nihal Developer <nihaltmdev@gmail.com>
2023-04-29 07:24:48 +0530

    added gui and page number selection rather than 1 or all

--- main.py
+++ main.py
@@ -3,25 +3,11 @@ import requests
 from requests.exceptions import HTTPError
 from rich.console import Console
 from rich.prompt import Prompt
-console = Console()
-console.print("Hello, Welcome to GetRFC", style="green on black")
-RFC = Prompt.ask("Enter the RFC Number", default="15")
-console.log(RFC)
-page=Prompt.ask("Do you want just the first page?", default="Y")
-if page.lower() == 'y':
-    page='fpage'
-else:
-    page='full'
-tofile=Prompt.ask("Do you want to write the output to a file", default="Y")
-if tofile.lower() == 'y':
-    tofile=True
-else:
-    tofile=False
-if tofile:
-    filename = input("Please enter the file name to store RFC at: ")
-else:
-    filename = None
-
+from tkinter import *
+import sys
+import time
+from tkinter.messagebox import showinfo
+from tkinter.simpledialog import askstring
 
 '''
 Python script to fetch RFC data from IETF's website using the requests
@@ -76,11 +62,8 @@ def runRequest(url):
 
     elif response.status_code == 403:
         raise ForbiddenError
-
-
-runRequest((getURL(RFC)))
-
-def getContentFromRFCNo(number, option, tofile, filename):
+    
+def getContentFromRFCNo(number, option, tofile, isgui, filename=''):
 
     out = ''
 
@@ -100,7 +83,7 @@ def getContentFromRFCNo(number, option, tofile, filename):
         out = data.text
 
 
-    elif option == 'fpage':
+    else:
 
         try:
             data = runRequest(getURL(number))
@@ -119,7 +102,7 @@ def getContentFromRFCNo(number, option, tofile, filename):
 
         for line in iterable_content:
             line = line.decode('utf-8')
-            if '[Page 1]' not in line:
+            if '[Page {}]'.format(option.strip()) not in line:
                 buf += line + '\n'
             else:
                 buf += line + '\n'
@@ -131,21 +114,81 @@ def getContentFromRFCNo(number, option, tofile, filename):
     if tofile:
 
         # we have been asked to write the output data to the file
+        if isgui:
+            with open(filename, 'w+') as current_file:
+                current_file.write(out)
 
-        if filename == '' or filename == None:
-            filename = input("Please enter the file name to store RFC at: ")
+        else:
             
-        with open(filename, 'w+') as current_file:
-            current_file.write(out)
+            with open(filename, 'w+') as current_file:
+                current_file.write(out)
 
-        print(f"Successfully written to file {filename} !")
-
+            print(f"Successfully written to file {filename} !")
     else:
+        if isgui:
+            showinfo(out)
+        else:
+            print(out)
 
-        print(out)
+def handler(RFC, pages, write_file):
+    if write_file:
+        filename = askstring("File name", "Enter name of file:")
+        getContentFromRFCNo(RFC, pages, False, True, filename)
+    else:
+        getContentFromRFCNo(RFC, pages, False, True)
 
 
-getContentFromRFCNo(RFC, page,tofile, filename) 
+if len(sys.argv)==2 and sys.argv[1]=="gui":
+    
+    root = Tk()
+    root.geometry("1200x800")
+    root.title("py-getrfc")
+    welcome_frame = Frame(root)
+    input_frame = Frame(root)
+    welcome_label = Label(welcome_frame, text='Welcome to py-getrfc!')
+    welcome_label.pack()
+    time.sleep(2)
+    welcome_frame.pack_forget()
+
+    rfc_label = Label(input_frame, text="Enter the RFC number:")
+    rfc_input = Entry(input_frame)
+    num_pages_label = Label(input_frame, text="Enter the number of pages:")
+    num_pages_input = Entry(input_frame)
+    rfc_label.pack()
+    rfc_input.pack()
+    num_pages_label.pack()
+    num_pages_input.pack()
+    var1=IntVar()
+    write_file = Checkbutton(input_frame, text="Write it to a file?", variable=var1, onvalue=1, offvalue=0)
+    write_file.var=BooleanVar(value=False)
+    write_file.pack()
+    submit_button = Button(input_frame, text="Submit", width = 10, height = 2, command=lambda:handler(rfc_input.get(), num_pages_input.get(), var1))
+    submit_button.pack()
+
+    input_frame.pack()
+    root.mainloop()
+
+else:
+    console = Console()
+    console.print("Hello, Welcome to GetRFC", style="green on black")
+    RFC = Prompt.ask("Enter the RFC Number", default="15")
+    console.log(RFC)
+    page=Prompt.ask("Do you want just the first page?", default="Y or number")
+    if page == 'y' or page == 'Y':
+        page='fpage'
+
+    tofile=Prompt.ask("Do you want to write the output to a file", default="Y")
+    if tofile.lower() == 'y':
+        tofile=True
+    else:
+        tofile=False
+    if tofile:
+        filename = input("Please enter the file name to store RFC at: ")
+    else:
+        filename = None
+
+    runRequest((getURL(RFC)))
+    getContentFromRFCNo(RFC, page,tofile, 0, filename) 
 # if __name__ == '__main__':
 
 #     # setup ArgParse for ease of use
