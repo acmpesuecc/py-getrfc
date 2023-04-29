e2000172a985f0e91a6c5337519696e74d891a22
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-04-29 13:21:48 +0530

    Add getURL_title() function

--- main.py
+++ main.py
@@ -5,25 +5,10 @@ from rich.console import Console
 from rich.prompt import Prompt
 import urllib.request
 import re
-
+import keyboard
+import time
 console = Console()
 console.print("Hello, Welcome to GetRFC", style="green on black")
-# RFC = Prompt.ask("Enter the RFC Number", default="15")
-# console.log(RFC)
-# page=Prompt.ask("Do you want just the first page?", default="Y")
-# if page.lower() == 'y':
-#     page='fpage'
-# else:
-#     page='full'
-# tofile=Prompt.ask("Do you want to write the output to a file", default="Y")
-# if tofile.lower() == 'y':
-#     tofile=True
-# else:
-#     tofile=False
-# if tofile:
-#     filename = input("Please enter the file name to store RFC at: ")
-# else:
-#     filename = None
 
 title = Prompt.ask("Enter the RFC title")
 console.log(title)
@@ -85,19 +70,39 @@ def getURL_title(title):
         for i in range(match, 1, -1):
             if data[i] == '\n' and data[i-1] == '\n':
                 return data[i+1:i+5]
-    
+
     ids = []
     for match in matches:
         ids.append(get_RFC_id(match))
         # yield runRequest(getURL((get_RFC_id(match))))
 
+    def get_RFC_title(match):
+        for i in range(match, 1, -1):
+            if data[i] == '\n' and data[i-1] == '\n':
+                for j in range(i+1, len(data)):
+                    if data[j:j+8] == '(Format:':
+                        return data[i+1:j].strip()
+
+    title = []
+    for match in matches:
+        title.append(get_RFC_title(match))
+    i = 0
+    while True:
+        if not title:
+            break
+        keyboard.wait('enter')
+        for j in range(i, i + 5):
+            if j < len(title):
+                print(title[j])
+        i += 5
 
     for i, id in enumerate(ids):
         print(f"{i+1}. {id}")
-        
+
     choice = Prompt.ask("Enter your choice", default="1")
     return getURL(ids[int(choice)-1])
     # return f'https://www.ietf.org/rfc/rfc{ids[int(choice)-1]}.txt'
+
 
 def runRequest(url):
 
@@ -121,77 +126,6 @@ def runRequest(url):
     elif response.status_code == 403:
         raise ForbiddenError
 
-
-# runRequest((getURL(RFC)))
-
-
-# def getContentFromRFCNo(number, option, tofile, filename):
-
-#     out = ''
-
-#     if option == 'full':
-
-#         try:
-#             data = runRequest(getURL(number))
-
-#         except Error404:
-#             print("Sorry! The resource you are looking for does not exist!")
-#             return 0
-
-#         except ForbiddenError:
-#             print("Sorry! It appears that you do not have access to the above URL")
-#             return 0
-
-#         out = data.text
-
-
-#     elif option == 'fpage':
-
-#         try:
-#             data = runRequest(getURL(number))
-
-#         except Error404:
-#             print("Sorry! The resource you are looking for does not exist!")
-#             return 0
-
-#         except ForbiddenError:
-#             print("Sorry! It appears that you do not have access to the above URL")
-#             return 0
-
-#         iterable_content = data.iter_lines()
-
-#         buf = ""
-
-#         for line in iterable_content:
-#             line = line.decode('utf-8')
-#             if '[Page 1]' not in line:
-#                 buf += line + '\n'
-#             else:
-#                 buf += line + '\n'
-#                 break
-
-#         out = buf
-
-
-#     if tofile:
-
-#         # we have been asked to write the output data to the file
-
-#         if filename == '' or filename == None:
-#             filename = input("Please enter the file name to store RFC at: ")
-            
-#         with open(filename, 'w+') as current_file:
-#             current_file.write(out)
-
-#         print(f"Successfully written to file {filename} !")
-
-#     else:
-
-#         print(out)
-
-
-# getContentFromRFCNo(RFC, page,tofile, filename)
-        
 runRequest(getURL_title(title))
 #Title
 def getContentFromRFCTitle(title, option, tofile, filename):
